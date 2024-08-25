597ef5927b5479526f69491599ff5b38ea077ac5
ainiazzah <aini.21006@mhs.unesa.ac.id>
2024-08-25 19:13:13 +0700

    add get spesific book data by id feature

--- src/handler.js
+++ src/handler.js
@@ -95,5 +95,30 @@ const getAllBooksHandler = () => {
     };
 };
 
+const getBookByIdHandler = (request, h) => {
+    const { bookId } = request.params;
 
-module.exports = { addBookHandler, getAllBooksHandler };
+    const book = books.filter((bk) => bk.id === bookId)[0];
+    
+    if(book !== undefined) {
+        return {
+            status: "success",
+            data: {
+                book,
+            },
+        };
+    }
+
+    const response = h.response({
+        status: "fail",
+        message: "Buku tidak ditemukan",
+    });
+    response.code(404);
+    return response;
+};
+
+module.exports = { 
+    addBookHandler, 
+    getAllBooksHandler,
+    getBookByIdHandler
+};

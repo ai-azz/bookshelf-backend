1e85a2ebe3e44f12547af5c41fa2373de678defd
ainiazzah <aini.21006@mhs.unesa.ac.id>
2024-08-26 22:52:33 +0700

    add get is finished read book

--- src/handler/getAllBook.js
+++ src/handler/getAllBook.js
@@ -1,13 +1,19 @@
 const books = require("../books");
 
 const getAllBooksHandler = (request, h) => {
-    const { name, reading } = request.query;
+    const { name, reading, finished } = request.query;
     let filteredBooks = books;
 
     if(reading !== undefined) {
         // filter book is reading or not
         const isReading = reading === "1";  // convert string '1' or '0' to boolean
         filteredBooks = filteredBooks.filter((book) => book.reading === isReading);
+    }
+
+    if(finished !== undefined) {
+        // filter book is finished reading or not yet
+        const isfinished = finished === "1";
+        filteredBooks = filteredBooks.filter((book) => book.finished === isfinished);
     }
     
     if(name) {

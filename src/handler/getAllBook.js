2db8657406f83f07e0ee1c2eb8a56f10173cb30f
ainiazzah <aini.21006@mhs.unesa.ac.id>
2024-08-26 22:49:40 +0700

    add get is reading book

--- src/handler/getAllBook.js
+++ src/handler/getAllBook.js
@@ -1,15 +1,22 @@
 const books = require("../books");
 
 const getAllBooksHandler = (request, h) => {
-    const { name } = request.query;
+    const { name, reading } = request.query;
     let filteredBooks = books;
 
+    if(reading !== undefined) {
+        // filter book is reading or not
+        const isReading = reading === "1";  // convert string '1' or '0' to boolean
+        filteredBooks = filteredBooks.filter((book) => book.reading === isReading);
+    }
+    
     if(name) {
         // filter book contains name, non-case sensitive
         filteredBooks = books.filter((book) =>
             book.name.toLowerCase().includes(name.toLowerCase())
         );
     }
+
 
     const responseBooks = filteredBooks.map((book) => ({
         id: book.id,
